f5ed008d6b79aed6307e4b759340e30230254f82
leang27 <52003343+leang27@users.noreply.github.com>
2021-09-13 10:43:30 +0700

    change comment

--- deploy/Presale.ts
+++ deploy/Presale.ts
@@ -10,7 +10,7 @@ type ArgsType = {
 
 const ARGS: {[chainId: string]: ArgsType} = {
   "97": {
-    tokenAddress: "0xDED2DEDf0cF48033cb50a4EF3e7587bAbc227151", // BNB
+    tokenAddress: "0xDED2DEDf0cF48033cb50a4EF3e7587bAbc227151", // KUM
     priceFeed: "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526", // BNB/USD
     supportedTokens: [
       {
