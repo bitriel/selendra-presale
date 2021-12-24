a2cf4dcbe871a608df3dc0ca1fbc9473ed6f4c99
leang27 <52003343+leang27@users.noreply.github.com>
2021-12-24 10:06:26 +0700

    remove compiled files

--- contracts/Balance.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0;
-
-contract Balance {
-    function balance(address con) external view returns(uint256) {
-        return con.balance;
-    }
-}
